81e08763851b86554c8fc8071296bb96d5befc05
Sebastian Westfal <mcskypl@gmail.com>
2021-04-30 11:42:47 +0200

    Update Search.js

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -77,10 +77,6 @@ const Search = ({currentIndex, setCurrentIndex, database, setDatabase}) => {
                 </div>
             }
 
-//             <div className="text-center fixed-bottom">
-//                 <Link className="btn btn-secondary" to="/updates">Aktualizacje</Link>
-//             </div>
-
             <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel"
                  aria-hidden="true">
                 <div className="modal-dialog">
